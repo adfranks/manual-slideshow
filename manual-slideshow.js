00612e2d8ea14567a75b763620676f145cec8b81
Adam Franks <52870365+adfranks@users.noreply.github.com>
2019-09-07 17:34:26 -0400

    Add html, css, js files via upload

---
+++ manual-slideshow.js
@@ -0,0 +1,100 @@
+/* Create a responsive slide show in a modal.  Criteria is it takes both
+portrait and landscape orientations and does not overflow the screen
+triggering the scroll bar. */
+var theModal = (function () {
+    var modal = document.getElementById("slide-modal"),
+    slides = document.getElementsByClassName("slide"),
+    modalImage = document.getElementById("modal-content"),
+    n = document.getElementById("next"),
+    p = document.getElementById("previous"),
+    f = document.getElementById("full"),
+    x = document.getElementById("close"),
+    slideNumber = 1;
+
+    // Make a click on the gallery image open the modal and display the image.
+    for (i = 0; i < slides.length; i++) {
+        slides[i].addEventListener("click", openModal); 
+    }
+
+    function openModal(event) {
+        modal.style.display = "block";
+        modalImage.src = event.target.src;
+        slideNumber = parseInt(event.target.id);
+    }
+
+    // Allow the modal to be closed easily.
+    window.addEventListener("click", clickClose);
+    function clickClose(event) {
+        if (event.target === modal || event.target === x) {
+            modal.style.display = "none";
+            closeFullScreen();
+        }
+    }
+
+    // Show and hide the next, previous, and close buttons.
+    window.addEventListener("mouseover", showControls);
+    window.addEventListener("mouseout", hideControls);
+    function showControls() {
+        n.style.opacity = 1;
+        p.style.opacity = 1;
+        f.style.opacity = 1;
+        x.style.opacity = 1;
+    }
+
+    function hideControls() {
+        n.style.opacity = 0;
+        p.style.opacity = 0;
+        f.style.opacity = 0;
+        x.style.opacity = 0;
+    }
+
+    // Activate the next and previous buttons.
+    window.addEventListener("click", addE);
+    function addE(event) {
+        if (event.target === n) {
+            changeSlide(1);
+        } else if (event.target === p) {
+            changeSlide(-1);
+        }
+    }
+
+    // Have the image source changed to display a different image.
+    function changeSlide(d) {
+        if (slideNumber == 1 && d === -1) {
+            slideNumber = slides.length;
+        } else if (slideNumber == (slides.length) && d === 1) {
+            slideNumber = 1;
+        } else {
+            slideNumber += d;
+        }
+        modalImage.src = document.getElementsByClassName("slide")[slideNumber -1].src;
+    }
+
+    // Make the full screen button work properly.
+    f.addEventListener("click", fullScreen);
+    function fullScreen() {
+        if (document.fullscreenElement) {
+            closeFullScreen();
+        } else if (modal.requestFullscreen) {
+            modal.requestFullscreen();
+        } else if (modal.mozRequestFullScreen) {
+            modal.mozRequestFullScreen();
+        } else if (modal.webkitRequestFullscreen) {
+            modal.webkitRequestFullscreen();
+        } else if (modal.msRequestFullscreen) {
+            modal.msRequestFullscreen();
+        }
+    }
+
+    function closeFullScreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (e.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (e.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (e.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+})(); 
