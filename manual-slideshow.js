9e399071a491e504b94280baf40c7ce3edd9117a
Adam Franks <52870365+adfranks@users.noreply.github.com>
2019-09-14 19:59:14 -0400

    fix swipe

--- manual-slideshow.js
+++ manual-slideshow.js
@@ -85,11 +85,13 @@ var theModal = (function () {
     
     modalImage.addEventListener("touchend", dirTouch);
     function dirTouch(event) {
+	if (ending === null) {return;}
 	if (ending < (beginning - 50)) {
 	    changeSlide(1);
 	} else if (ending > (beginning + 50)) {
 	    changeSlide(-1)
 	}
+	ending = null;
     }
 
     // Make the full screen button work properly.
